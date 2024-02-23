54409f9f22c6b32a5f623e990ca404481b1d6f26
Anton Masyuk <a.masyuk@comagic.dev>
2024-02-23 17:27:41 +0500

    change method of allowed updates

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import atexit
 import logging.config
 
+from aiogram.types import BotCommand, BotCommandScopeAllPrivateChats
 from aiohttp import web
 import ssl
 import yaml
@@ -11,11 +12,51 @@ from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_applicati
 
 from handlers import router
 from config import settings
-from utils import on_startup, on_shutdown
 
 
 def main() -> None:
     dp = Dispatcher()
+
+    async def on_startup(bot: Bot) -> None:
+        logger = logging.getLogger(__name__)
+        res = await bot.set_webhook(
+            f"https://{settings.DOMAIN}{settings.WEBHOOK_PATH}",
+            secret_token=settings.WEBHOOK_SECRET,
+            drop_pending_updates=True,
+            allowed_updates=dp.resolve_used_update_types(),
+
+        )
+        if res:
+            logger.info("Webhook installed successfully")
+            for admin_id in settings.ADMINS:
+                try:
+                    await bot.send_message(chat_id=admin_id, text="Bot is online!")
+                    logger.info(f"Message to admin {admin_id} was sent.")
+                except Exception as e:
+                    logger.warning(f"'{e}' was occurred, chat id is {admin_id}.")
+        else:
+            logger.error("Webhook was not installed")
+
+        await bot.set_my_commands(
+            [
+                BotCommand(
+                    command="start",
+                    description="Начало работы с ботом."
+                ),
+                BotCommand(
+                    command="help",
+                    description="Бот домашний помощник."
+                )
+            ],
+            scope=BotCommandScopeAllPrivateChats(),
+        )
+
+    async def on_shutdown(bot: Bot) -> None:
+        logger = logging.getLogger(__name__)
+        res = await bot.delete_webhook()
+        if res:
+            logger.info("Webhook deleted successfully")
+
     dp.include_router(router)
 
     dp.startup.register(on_startup)
--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,6 +1,5 @@
-from .startup_shutdown import on_shutdown, on_startup
 from .message_renderer import render_message
 from .prepare_message import prepare_message
 
 
-__all__ = [on_shutdown, on_startup, render_message, prepare_message]
+__all__ = [render_message, prepare_message]
